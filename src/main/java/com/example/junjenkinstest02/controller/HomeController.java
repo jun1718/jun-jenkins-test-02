f464f7d4d5a3e1f85f70e5e25dbbddcad519abf7
jun1718 <gbeovhsqhtka@naver.com>
2023-01-02 22:21:58 +0900

    [feat] : json 반환값 안녕하세요2로 변경

--- src/main/java/com/example/junjenkinstest02/controller/HomeController.java
+++ src/main/java/com/example/junjenkinstest02/controller/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public String home() {
-        return "안녕하세요";
+        return "안녕하세요2";
     }
 }
