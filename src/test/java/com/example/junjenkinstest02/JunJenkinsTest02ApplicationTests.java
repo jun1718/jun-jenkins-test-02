463ad19f42ccc2ee04aae1ad9e00f74cdb64d2ef
jun1718 <gbeovhsqhtka@naver.com>
2023-01-02 21:22:14 +0900

    Initial commit

---
+++ src/test/java/com/example/junjenkinstest02/JunJenkinsTest02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junjenkinstest02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunJenkinsTest02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
